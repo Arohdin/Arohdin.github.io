cdb32d28e7acf7faf8c66eb12701ac93b072d91f
Adam Rohdin <adaro815@student.liu.se>
2017-10-09 16:14:46 +0200

    fade-in

---
+++ script.js
@@ -0,0 +1,6 @@
+$(document).ready(() => {
+    $('#image').hide().fadeIn(500);
+    $('#header').hide().fadeIn(800);
+    $('#content').hide().fadeIn(1000);
+    $('#socials').hide().fadeIn(1300);
+});
