0a4c0fa3220e0d908ca93dc7316d6fab766f412e
Adam <adam.rohdin@outlook.com>
2017-10-09 22:16:27 +0200

    somewhat responsive

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 $(document).ready(() => {
     $('#image').hide().fadeIn(500);
+    $('#quote').hide().fadeIn(3000);
     $('#header').hide().fadeIn(800);
     $('#content').hide().fadeIn(1000);
     $('#socials').hide().fadeIn(1300);
