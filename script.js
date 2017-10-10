a3cf73109ad19c510e5bda31d014b5b5507d3cbe
Adam Rohdin <adam.rohdin@outlook.com>
2017-10-10 09:05:01 +0200

    fade in timers

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
 $(document).ready(() => {
-    $('#image').hide().fadeIn(500);
-    $('#quote').hide().fadeIn(3000);
-    $('#header').hide().fadeIn(800);
-    $('#content').hide().fadeIn(1000);
-    $('#socials').hide().fadeIn(1300);
+    $('#header').hide();
+    $('#image').hide();
+    $('#info').hide();
+    $('#contact').hide();
+    $('#socials').hide();
+    $('#quoteText').hide();
+
+    $('#image').fadeIn(1000);
+    setTimeout(() => {
+        $('#header').fadeIn(1000);
+        setTimeout(() => {
+            $('#info').fadeIn(1000);
+            setTimeout(() => {
+                $('#contact').hide().fadeIn(1000);
+                $('#socials').hide().fadeIn(1000);
+                setTimeout(() => {
+                    $('#quoteText').hide().fadeIn(1500);
+                },750);
+            }, 250);
+        }, 250);
+    }, 250);
+
 });
